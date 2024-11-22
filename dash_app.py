7bc715ede1bd331d64e4812ded0bd4ce9ed4d890
monaswarnakar <132899393+monaswarnakar@users.noreply.github.com>
2024-11-22 11:25:53 +0530

    Update dash_app.py

--- dash_app.py
+++ dash_app.py
@@ -127,6 +127,7 @@ fig.update_layout(
 # Initialize Dash app with suppress_callback_exceptions=True
 app = dash.Dash(__name__)
 server = app.server
+
 # Define transportation modes and years for the dropdown
 transport_modes = ['Subways', 'Buses', 'LIRR', 'Metro-North', 'Access-A-Ride', 'Bridges and Tunnels', 'Staten Island Railway']
 years = sorted(data['Year'].unique())
